22e6144178bd4f5a91687999f31bd843d1f56629
Mufti011 <140704062+Mufti011@users.noreply.github.com>
2023-07-27 16:48:28 +0700

    Add files via upload

---
+++ .vscode/Bitwise.py
@@ -0,0 +1,52 @@
+print("======BITWISE======")
+
+#operator bitwise, operasi biner, binary
+
+a = 5
+b = 9
+
+#bitwise or 
+c = a | b
+print("======OR======")
+print('nilai :',a,',binary :',format(a,'08b'))
+
+print('nilai :',b,',binary :',format(b,'08b'))
+print('-----------------------------------[|]')
+
+print('nilai :',c,',binary :',format(c,'08b'))
+
+
+#bitwise and
+c = a and b
+print("======AND======")
+print('nilai :',a,',binary :',format(a,'08b'))
+
+print('nilai :',b,',binary :',format(b,'08b'))
+print('-----------------------------------[|]')
+
+print('nilai :',c,',binary :',format(c,'08b'))
+
+
+#bitwise XOR 
+c = a ^ b
+print("======XOR======")
+print('nilai :',a,',binary :',format(a,'08b'))
+
+print('nilai :',b,',binary :',format(b,'08b'))
+print('-----------------------------------[|]')
+
+print('nilai :',c,',binary :',format(c,'08b'))
+
+
+#bitwise NOT ~
+c = ~a
+print("======NOT ~======")
+print('nilai :',a,',binary :',format(a,'08b'))
+
+print('nilai :',b,',binary :',format(b,'08b'))
+print('-----------------------------------[|]')
+
+print('nilai :',c,',binary :',format(c,'08b'))
+
+
+print("=============TERIMAKASIH==============")
