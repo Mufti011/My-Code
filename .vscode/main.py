38ee78bdcbcb0ce6424f538787fd145977b0b76d
Mufti011 <140704062+Mufti011@users.noreply.github.com>
2023-07-27 09:35:39 +0000

    Pending changes exported from your codespace

---
+++ .vscode/main.py
@@ -0,0 +1,6 @@
+print("=====Rumus Luas Persegi Panjang=====")
+#rumus luas panjang x lebar
+panjang = float(input("Masukan panjang = "))
+lebar = float(input("Masukan lebar = "))
+luas = panjang * lebar
+print('Luas persegi panjang adalah ', luas)
